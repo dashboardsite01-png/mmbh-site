53bdc9306f3ed3fbddf9718075b60ed09eb8f1b1
dashboardsite01-png <dashboard.site.01@gmail.com>
2026-02-04 19:23:55 -0300

    Update page.tsx

--- src/app/apresentacao/page.tsx
+++ src/app/apresentacao/page.tsx
@@ -1,1 +1,15 @@
 
+import Link from 'next/link';
+import React from 'react'
+
+function Apresentacao() {
+  return (
+    <div className='h-screen w-full bg-white text-2xl text-blue-500'>
+<Link href="/">
+Home
+</Link>
+    </div>
+  )
+}
+
+export default Apresentacao
